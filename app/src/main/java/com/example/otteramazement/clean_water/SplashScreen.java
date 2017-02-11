b0b3affa495063b25547c284608fbf82afa0384f
varmendari14 <varmendari14@gmail.com>
2017-02-11 14:59:15 -0500

    Violet SPlash Screen WORKS

---
+++ app/src/main/java/com/example/otteramazement/clean_water/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.otteramazement.clean_water;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Violet on 2/11/2017.
+ */
+
+public class SplashScreen extends Activity{
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(SPLASH_DISPLAY_LENGTH);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
