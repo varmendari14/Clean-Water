49bfa6efd6bdbaa9f1508784860ca3d9d8dcd967
varmendari14 <varmendari14@gmail.com>
2017-02-11 23:11:45 -0500

    Welcome Basic

---
+++ app/src/main/java/com/example/otteramazement/clean_water/WelcomeActivity.java
@@ -0,0 +1,64 @@
+package com.example.otteramazement.clean_water;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Violet on 2/11/2017.
+ */
+
+public class WelcomeActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/PAPYRUS.TTF");
+
+        TextView loginButton = (TextView) findViewById(R.id.welcome_login);
+        loginButton.setTypeface(font);
+        TextView registerButton = (TextView) findViewById(R.id.welcome_register);
+        registerButton.setTypeface(font);
+
+        // Set up the login form.
+
+
+
+//        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+//        populateAutoComplete();
+//
+//        mPasswordView = (EditText) findViewById(R.id.password);
+//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+//                    attemptLogin();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//
+//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+//        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                attemptLogin();
+//            }
+//        });
+//
+//        mLoginFormView = findViewById(R.id.login_form);
+//        mProgressView = findViewById(R.id.login_progress);
+    }
+
+}
