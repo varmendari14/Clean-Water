82737661d78c4c90e1531911bd425d4a6b23dc1f
Mary Prouty <meprouty@gatech.edu>
2017-02-12 17:29:39 -0500

    Activity for post-login

---
+++ app/src/main/java/com/example/otteramazement/clean_water/AppActivity.java
@@ -0,0 +1,46 @@
+package com.example.otteramazement.clean_water;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by maryprouty on 2/12/17.
+ */
+
+public class AppActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+
+        //Typeface font = Typeface.createFromAsset(getAssets(), "fonts/PAPYRUS.TTF");
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //TextView logoutButton = (TextView) findViewById(R.id.welcome_loginbutton_textView);
+        //logoutButton.setTypeface(font);
+
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), WelcomeActivity.class));
+            }
+        });
+
+    }
+
+}
