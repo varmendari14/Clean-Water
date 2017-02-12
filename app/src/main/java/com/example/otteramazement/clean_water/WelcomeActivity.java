1ddfbc7cbb78f4cd87f780f92af7c891bc58774f
varmendari14 <varmendari14@gmail.com>
2017-02-12 18:59:57 -0500

    Comments Gone

--- app/src/main/java/com/example/otteramazement/clean_water/WelcomeActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/WelcomeActivity.java
@@ -42,34 +42,6 @@ public class WelcomeActivity extends Activity{
                 startActivity(new Intent(getBaseContext(), LoginActivity.class));
             }
         });
-
-
-
-//        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-//        populateAutoComplete();
-//
-//        mPasswordView = (EditText) findViewById(R.id.password);
-//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-//                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-//                    attemptLogin();
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//
-//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-//        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                attemptLogin();
-//            }
-//        });
-//
-//        mLoginFormView = findViewById(R.id.login_form);
-//        mProgressView = findViewById(R.id.login_progress);
     }
 
 }
