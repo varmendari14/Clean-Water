8ea165e398eda3f3781ed95f7c38d9f1c0e261e8
Emma <eeggleston3@gatech.edu>
2017-02-20 10:39:07 -0500

    created inheritance for different types of users

---
+++ app/src/main/java/com/example/otteramazement/clean_water/Manager.java
@@ -0,0 +1,11 @@
+package com.example.otteramazement.clean_water;
+
+/**
+ * Created by Emma on 2/20/2017.
+ */
+
+public class Manager extends UserProfile {
+    public Manager(ProfileType profileType, String name, String username, String password) {
+        super(profileType, name, username, password);
+    }
+}
---
+++ app/src/main/java/com/example/otteramazement/clean_water/Worker.java
@@ -0,0 +1,11 @@
+package com.example.otteramazement.clean_water;
+
+/**
+ * Created by Emma on 2/20/2017.
+ */
+
+public class Worker extends UserProfile {
+    public Worker(ProfileType profileType, String name, String username, String password) {
+        super(profileType, name, username, password);
+    }
+}
