970597ef0cab0eb25b451511f8a1e6f48598faed
varmendari14 <varmendari14@gmail.com>
2017-02-20 10:08:20 -0500

    Now IS case sensitive, also USER is what is accepted as login

--- app/src/main/java/com/example/otteramazement/clean_water/LoginActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/LoginActivity.java
@@ -186,7 +186,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString().toLowerCase();
+        String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
