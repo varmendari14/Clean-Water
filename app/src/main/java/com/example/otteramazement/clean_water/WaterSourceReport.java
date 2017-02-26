7364f7509ae2a1fae0b1cf7b64aec6cf580dd10a
Mary Prouty <meprouty@gatech.edu>
2017-02-26 18:08:54 -0500

    Model for the water source report

---
+++ app/src/main/java/com/example/otteramazement/clean_water/WaterSourceReport.java
@@ -0,0 +1,62 @@
+package com.example.otteramazement.clean_water;
+
+import java.util.Random;
+
+/**
+ * Created by Mary Prouty on 2/26/17.
+ */
+
+public class WaterSourceReport {
+
+    private Random rand = new Random();
+    private int reportNum;
+
+    private String date;
+    private String time;
+    private String reporter;
+    private String location;
+
+    //No-arg constructor creates random number for report
+
+    public WaterSourceReport() {
+        reportNum = rand.nextInt(10000);
+    }
+
+    //Getters and setters for report data
+
+    public void setDate(String aDate) {
+        date = aDate;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setTime(String aTime) {
+        time = aTime;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setReporter(String userName) {
+        reporter = userName;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setLocation(String loc) {
+        location = loc;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getReportNumber() {
+        return reportNum;
+    }
+}
