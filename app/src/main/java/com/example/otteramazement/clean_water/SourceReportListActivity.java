5d6f54d080406cc7c2115c5a1653839ab4dfd3d2
Mary Prouty <meprouty@gatech.edu>
2017-02-26 20:28:34 -0500

    User data passes through

--- app/src/main/java/com/example/otteramazement/clean_water/SourceReportListActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/SourceReportListActivity.java
@@ -15,12 +15,13 @@ import android.widget.TextView;
 public class SourceReportListActivity extends Activity {
 
 
-    //UserProfile _user;
-    //WaterSourceReport _report = new WaterSourceReport();
+    UserProfile _user;
 
     public static final String ARG_USER = "user";
 
     protected void onCreate(Bundle savedInstanceState) {
+
+        _user = (UserProfile) getIntent().getSerializableExtra(SourceReportChoiceActivity.ARG_USER);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_source_report_list);
@@ -38,7 +39,7 @@ public class SourceReportListActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent backIntent = new Intent(getBaseContext(), SourceReportChoiceActivity.class);
-                //backIntent.putExtra(WaterReportActivity.ARG_USER, _user);
+                backIntent.putExtra(SourceReportListActivity.ARG_USER, _user);
                 startActivity(backIntent);
             }
         });
