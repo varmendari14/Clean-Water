80ded93620b9e84d748cbc078b293a29c837c155
varmendari14 <varmendari14@gmail.com>
2017-03-01 09:45:03 -0500

    has a tostring method now

--- app/src/main/java/com/example/otteramazement/clean_water/WaterType.java
+++ app/src/main/java/com/example/otteramazement/clean_water/WaterType.java
@@ -2,6 +2,7 @@ package com.example.otteramazement.clean_water;
 
 /**
  * Created by Emma on 2/28/2017.
+ * Authors: Emma, Violet
  */
 
 public enum WaterType {
@@ -12,21 +13,21 @@ public enum WaterType {
      * @param type  String name of water type
      * @return      Corresponding water type
      */
-    public static WaterType getWaterTypeFromString(String type) {
+    public static String waterTypeToString(WaterType type) {
         switch(type) {
             default:
-            case "BOTTLED":
-                return WaterType.BOTTLED;
-            case "WELL":
-                return WaterType.WELL;
-            case "STREAM":
-                return WaterType.STREAM;
-            case "LAKE":
-                return WaterType.LAKE;
-            case "SPRING":
-                return WaterType.SPRING;
-            case "OTHER":
-                return WaterType.OTHER;
+            case BOTTLED:
+                return "BOTTLED";
+            case WELL:
+                return "WELL";
+            case STREAM:
+                return "STREAM";
+            case LAKE:
+                return "LAKE";
+            case SPRING:
+                return "SPRING";
+            case OTHER:
+                return "OTHER";
         }
     }
 }
