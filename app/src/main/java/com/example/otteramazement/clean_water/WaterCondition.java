074893b84f5b430ff58bf0732f3caa947cf9c1c5
varmendari14 <varmendari14@gmail.com>
2017-03-01 09:47:15 -0500

    has a tostring method now

--- app/src/main/java/com/example/otteramazement/clean_water/WaterCondition.java
+++ app/src/main/java/com/example/otteramazement/clean_water/WaterCondition.java
@@ -2,6 +2,7 @@ package com.example.otteramazement.clean_water;
 
 /**
  * Created by Emma on 2/28/2017.
+ * Authors: Emma, Violet
  */
 
 public enum WaterCondition {
@@ -12,17 +13,17 @@ public enum WaterCondition {
      * @param type  String name of condition
      * @return      Corresponding condition
      */
-    public static WaterCondition getConditionFromString(String type) {
+    public static String waterContiditionToString(WaterCondition type) {
         switch(type) {
             default:
-            case "WASTE":
-                return WaterCondition.WASTE;
-            case "TREATABLE_MUDDY":
-                return WaterCondition.TREATABLE_MUDDY;
-            case "TREATABLE_CLEAR":
-                return WaterCondition.TREATABLE_CLEAR;
-            case "POTABLE":
-                return WaterCondition.POTABLE;
+            case WASTE:
+                return "WASTE";
+            case TREATABLE_MUDDY:
+                return "TREATABLE_MUDDY";
+            case TREATABLE_CLEAR:
+                return "TREATABLE_CLEAR";
+            case POTABLE:
+                return "POTABLE";
         }
     }
 }
