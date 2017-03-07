9d1361286c7c1e3a4e4131a813caf528d7540d2d
Emma <eeggleston3@gatech.edu>
2017-03-07 13:05:39 -0500

    javadoc

--- app/src/main/java/com/example/otteramazement/clean_water/OurHashMap.java
+++ app/src/main/java/com/example/otteramazement/clean_water/OurHashMap.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
  * Created by Emma on 2/28/2017.
  */
 
+/**
+ * hash map that holds all the users with username and password
+ */
 public class OurHashMap {
     static HashMap<String, UserProfile> userMap = new HashMap<>();
 }
