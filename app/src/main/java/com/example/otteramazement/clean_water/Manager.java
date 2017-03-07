2f282a9b11829704b75cf6a3969cde7383b470fb
Emma <eeggleston3@gatech.edu>
2017-03-07 13:02:07 -0500

    created list for

--- app/src/main/java/com/example/otteramazement/clean_water/Manager.java
+++ app/src/main/java/com/example/otteramazement/clean_water/Manager.java
@@ -6,11 +6,28 @@ package com.example.otteramazement.clean_water;
  * Authors: Emma
  */
 
+/**
+ * This class represents the manager which is a subclass of user
+ */
 public class Manager extends UserProfile {
     public Manager(ProfileType profileType, String name, String username, String password) {
         super(profileType, name, username, password);
     }
 
+    /**
+     * constructor for manager
+     * @param profileType manager
+     * @param name name of user
+     * @param username username of user
+     * @param password password
+     * @param email email
+     * @param address street address
+     * @param city city address
+     * @param state state
+     * @param country country
+     * @param phoneNumber phone number
+     * @param title dr, mr, mrs, etc
+     */
     public Manager(ProfileType profileType, String name, String username, String password,
                  String email, String address, String city, String state, String country,
                  String phoneNumber, String title) {
