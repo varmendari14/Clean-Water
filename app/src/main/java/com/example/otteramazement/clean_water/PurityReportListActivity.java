8cca95c00b9705324a564018fdc1ea38e40f853b
Mary Prouty <meprouty@gatech.edu>
2017-03-07 21:42:30 -0500

    Just some housekeeping stuff

--- app/src/main/java/com/example/otteramazement/clean_water/PurityReportListActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/PurityReportListActivity.java
@@ -16,14 +16,8 @@ import android.widget.TextView;
 
 public class PurityReportListActivity extends Activity {
 
-
-    UserProfile _user;
-
-    public static final String ARG_USER = "user";
-
     protected void onCreate(Bundle savedInstanceState) {
 
-        _user = (UserProfile) getIntent().getSerializableExtra(SourceReportChoiceActivity.ARG_USER);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purity_report_list);
@@ -41,7 +35,6 @@ public class PurityReportListActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent backIntent = new Intent(getBaseContext(), PurityReportChoiceActivity.class);
-                backIntent.putExtra(PurityReportListActivity.ARG_USER, _user);
                 startActivity(backIntent);
             }
         });
