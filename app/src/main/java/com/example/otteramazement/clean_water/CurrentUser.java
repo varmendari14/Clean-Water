169097673633bdd8bcdc9a16a2a877a8806e14c7
Mary Prouty <meprouty@gatech.edu>
2017-03-07 21:40:58 -0500

    Singleton for current user info

---
+++ app/src/main/java/com/example/otteramazement/clean_water/CurrentUser.java
@@ -0,0 +1,11 @@
+package com.example.otteramazement.clean_water;
+
+import java.util.LinkedList;
+
+/**
+ * Created by maryprouty on 3/7/17.
+ */
+
+public class CurrentUser {
+    static LinkedList<UserProfile> currentUser = new LinkedList();
+}
