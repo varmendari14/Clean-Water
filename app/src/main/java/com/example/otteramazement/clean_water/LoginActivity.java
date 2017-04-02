0681df4835ab7a1edcb272f5d8f1eaacadffb095
Emma <eeggleston3@gatech.edu>
2017-04-01 23:27:31 -0400

    style stuff for login

--- app/src/main/java/com/example/otteramazement/clean_water/LoginActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/LoginActivity.java
@@ -1,39 +1,39 @@
 package com.example.otteramazement.clean_water;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Typeface;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.app.LoaderManager.LoaderCallbacks;
+        import android.annotation.TargetApi;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.graphics.Typeface;
+        import android.support.annotation.NonNull;
+        import android.support.design.widget.Snackbar;
+        import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
+        import android.content.CursorLoader;
+        import android.content.Loader;
+        import android.database.Cursor;
+        import android.net.Uri;
+        import android.os.AsyncTask;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.provider.ContactsContract;
+        import android.text.TextUtils;
+        import android.view.KeyEvent;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.view.inputmethod.EditorInfo;
+        import android.widget.ArrayAdapter;
+        import android.widget.AutoCompleteTextView;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
-import android.widget.ImageView;
+        import static android.Manifest.permission.READ_CONTACTS;
+        import android.widget.ImageView;
 
 //Otter Amazement (Team 11)
 //Authors Mary, Avery, Cat, Violet, Emma
