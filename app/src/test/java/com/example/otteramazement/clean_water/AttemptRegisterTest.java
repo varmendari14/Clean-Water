3fcfb39620706cfb57d1b2d622cac3c9d70d0076
Emma <eeggleston3@gatech.edu>
2017-04-03 11:24:24 -0400

    updated Emma's Junits

--- app/src/test/java/com/example/otteramazement/clean_water/AttemptRegisterTest.java
+++ app/src/test/java/com/example/otteramazement/clean_water/AttemptRegisterTest.java
@@ -1,6 +1,7 @@
 package com.example.otteramazement.clean_water;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -13,27 +14,32 @@ public class AttemptRegisterTest {
 
     private final RegisterActivity r = new RegisterActivity();
 
+    @Before
+    public void beforeTest() {
+        OurHashMap.userMap.remove("User");
+    }
+
     @Test
     public void passMatch() {
-        r.register("password", "password", "User", "User", ProfileType.USER);
+        r.register("password", "password", "User", "User", ProfileType.USER, false);
         Assert.assertTrue("This should register a user.", OurHashMap.userMap.containsKey("User"));
     }
 
     @Test
     public void passNotMatch() {
-        r.register("password", "pass", "User", "User", ProfileType.ADMIN);
+        r.register("password", "pass", "User", "User", ProfileType.ADMIN, false);
         Assert.assertTrue("This should not register a user because the passwords do not match.", !OurHashMap.userMap.containsKey("User"));
     }
 
     @Test
     public void userShort() {
-        r.register("password", "password", "Us", "Us", ProfileType.WORKER);
+        r.register("password", "password", "Us", "Us", ProfileType.WORKER, false);
         Assert.assertTrue("This should not register a user because the username is too short.", !OurHashMap.userMap.containsKey("Us"));
     }
 
     @Test
     public void nameShort() {
-        r.register("pass", "pass", "User", "", ProfileType.ADMIN);
+        r.register("pass", "pass", "User", "A", ProfileType.ADMIN, false);
         Assert.assertTrue("This should not register a user because the name is too short.", !OurHashMap.userMap.containsKey("User"));
     }
 }
