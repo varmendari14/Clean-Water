8ebed2361897eee58d2ee54634a40cd9a36d71b8
Emma <eeggleston3@gatech.edu>
2017-04-06 12:30:33 -0400

    fixing google maps back

--- app/src/main/java/com/example/otteramazement/clean_water/MapsActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/MapsActivity.java
@@ -78,9 +78,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         // Add a marker for each water source report
         List<WaterSourceReport> reportList = WaterReportList.waterSourceList;
