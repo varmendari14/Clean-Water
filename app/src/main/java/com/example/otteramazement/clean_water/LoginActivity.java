aa8e28755e4397dc2941d3c2f13f7b3f0e0bf7b6
Emma <eeggleston3@gatech.edu>
2017-04-10 00:11:54 -0400

    style fix

--- app/src/main/java/com/example/otteramazement/clean_water/LoginActivity.java
+++ app/src/main/java/com/example/otteramazement/clean_water/LoginActivity.java
@@ -165,7 +165,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if (requestCode == REQUEST_READ_CONTACTS) {
-            if ((grantResults.length == 1) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            if ((grantResults.length == 1)
+                && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 populateAutoComplete();
             }
         }
